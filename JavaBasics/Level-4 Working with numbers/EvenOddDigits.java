d3085e32a2d4454c4245455784408150c5376ac5
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-25 15:13:13 +0530

    Remove IntelliJ generated files

--- JavaBasics/Level-4 Working with numbers/EvenOddDigits.java
+++ JavaBasics/Level-4 Working with numbers/EvenOddDigits.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+//Write a program to count the number of even digits and odd digits in a number n.
+import java.util.*;
 
 public class EvenOddDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int n = sc.nextInt();
+
+    }
 }
