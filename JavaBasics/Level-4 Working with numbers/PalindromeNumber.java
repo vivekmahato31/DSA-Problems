869f2da7ea4ecd9b203c0d4b97ccac2e2150ee09
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-07-28 10:56:59 +0530

    Solved problem PalindromeNumber and edited EvenOddDigits and SumOfDigits program

--- JavaBasics/Level-4 Working with numbers/PalindromeNumber.java
+++ JavaBasics/Level-4 Working with numbers/PalindromeNumber.java
@@ -4,24 +4,27 @@ import java.util.*;
 public class PalindromeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter Number: ");
         int n = sc.nextInt();
+        n = Math.abs(n);
+
         int original = n;
+        int reverse = 0;
 
         while(n>0){
 
             int digit = n % 10;
+            reverse = reverse * 10 + digit;
             n = n/10;
-            digit++;
-
-            if(digit == original){
-                System.out.println(n + " is a Palindrome Number");
-            }
-            else {
-                System.out.println(n + " is a Not Palindrome Number");
-
-            }
         }
+
+        if (reverse == original) {
+            System.out.println(original + " is a Palindrome Number");
+        } else {
+            System.out.println(original + " is Not a Palindrome Number");
+        }
+
         sc.close();
     }
 }
--- JavaBasics/Level-4 Working with numbers/SumOfDigits.java
+++ JavaBasics/Level-4 Working with numbers/SumOfDigits.java
@@ -5,18 +5,18 @@ import java.util.*;
 public class SumOfDigits {
     public static void main (String [] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter n: ");
+        System.out.print("Enter n: ");
         int n =sc.nextInt();
         int original = n;
         n = Math.abs(n);
         int sum = 0 ;
 
         if ( n == 0){
-            System.out.println("The sum of all digits of a number "+ original + " is: ");
+            System.out.print("The sum of all digits of a number "+ original + " is: ");
             System.out.println("0");
         }
         else {
-                System.out.println("The sum of all digits of a number "+ original + " is: ");
+                System.out.print("The sum of all digits of a number "+ original + " is: ");
             while ( n > 0){
                 int digit = n % 10;
                 n = n/10;
