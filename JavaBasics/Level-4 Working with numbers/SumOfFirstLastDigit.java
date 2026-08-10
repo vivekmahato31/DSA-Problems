efe60a8693df2ff11555650b237b23c8479fd8c7
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-08-10 09:06:02 +0530

    level 4 completed.

--- JavaBasics/Level-4 Working with numbers/SumOfFirstLastDigit.java
+++ JavaBasics/Level-4 Working with numbers/SumOfFirstLastDigit.java
@@ -1,10 +1,26 @@
 //Write a program to find the sum of the first and last digit of a number n.
 
 import java.util.*;
-public class SumOfFirstLastDigit {
-    public static void main() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter The Number: ");
 
+public class SumOfFirstLastDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter The Number: ");
+        int n = sc.nextInt();
+
+        n = Math.abs(n);
+
+        int LastDigit = n % 10;
+
+        while (n >= 10){
+            n = n/10;
+        }
+        int FirstDigit = n;
+
+        int sum = FirstDigit + LastDigit;
+        System.out.print("The Sum Of First and Last Digit is " + sum);
+
+        sc.close();
     }
 }
