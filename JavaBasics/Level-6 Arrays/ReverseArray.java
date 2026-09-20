fbe3d9810f334fa2f821caf1c15a5f3ec80895d2
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-09-20 21:40:05 +0530

    Solved some more question

---
+++ JavaBasics/Level-6 Arrays/ReverseArray.java
@@ -0,0 +1,27 @@
+//Reverse the elements of an array.
+
+
+import java.util.*;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Reverse array:");
+
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ JavaBasics/Level-6 Arrays/SecondLargest.java
@@ -0,0 +1,37 @@
+//Second Largest Element
+
+import java.util.*;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Largest = " + largest);
+        System.out.println("Second Largest = " + secondLargest);
+    }
+}
