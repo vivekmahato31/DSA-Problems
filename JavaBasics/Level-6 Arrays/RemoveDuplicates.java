588c763d9db88ca618bd377018614c49d96275b8
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-09-20 22:05:00 +0530

    Solved some more question

---
+++ JavaBasics/Level-6 Arrays/RemoveDuplicates.java
@@ -0,0 +1,40 @@
+//Remove Duplicate Elements
+
+import java.util.*;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array without duplicates:");
+
+        for (int i = 0; i < n; i++) {
+
+            boolean duplicate = false;
+
+            for (int j = 0; j < i; j++) {
+
+                if (arr[i] == arr[j]) {
+                    duplicate = true;
+                    break;
+                }
+            }
+
+            if (!duplicate) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
