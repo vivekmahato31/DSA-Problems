6f26505e19082b78ea05f2c1b862041ea0a6059f
vivekmahato31 <181303045+vivekmahato31@users.noreply.github.com>
2026-09-20 22:06:58 +0530

    Solved some more question

---
+++ JavaBasics/Level-6 Arrays/MergeArrays.java
@@ -0,0 +1,48 @@
+//Merge Two Arrays
+
+import java.util.*;
+
+public class MergeArrays {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of first array: ");
+        int n1 = sc.nextInt();
+
+        int[] arr1 = new int[n1];
+
+        System.out.println("Enter first array:");
+
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter size of second array: ");
+        int n2 = sc.nextInt();
+
+        int[] arr2 = new int[n2];
+
+        System.out.println("Enter second array:");
+
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        int[] merged = new int[n1 + n2];
+
+        for (int i = 0; i < n1; i++) {
+            merged[i] = arr1[i];
+        }
+
+        for (int i = 0; i < n2; i++) {
+            merged[n1 + i] = arr2[i];
+        }
+
+        System.out.println("Merged array:");
+
+        for (int i = 0; i < merged.length; i++) {
+            System.out.print(merged[i] + " ");
+        }
+    }
+}
